09b4325a4fc7fecbd72a1d23f830622f15933d3e
viethetthoi <130229331+viethetthoi@users.noreply.github.com>
2024-09-17 20:46:49 +0700

    convert_image_new

--- Train/image_train.py
+++ Train/image_train.py
@@ -10,25 +10,16 @@ destination_dir = 'D:/TrafficSignRecognition_ComputerVision/Train/coco128/images
 # Tạo thư mục đích nếu nó chưa tồn tại
 os.makedirs(destination_dir, exist_ok=True)
 
-# Khởi tạo số đếm cho các file ảnh
-image_counter = 1
-
 # Lặp qua tất cả các thư mục con trong thư mục nguồn
 for subdir, _, files in os.walk(source_dir):
     for file in files:
         # Xây dựng đường dẫn đầy đủ cho file ảnh nguồn
         file_path = os.path.join(subdir, file)
         
-        # Tạo tên mới cho file ảnh
-        new_filename = f'image{image_counter}.jpg'
+        # Xây dựng đường dẫn đầy đủ cho file ảnh đích (giữ nguyên tên tệp)
+        destination_path = os.path.join(destination_dir, file)
         
-        # Xây dựng đường dẫn đầy đủ cho file ảnh đích
-        destination_path = os.path.join(destination_dir, new_filename)
-        
-        # Sao chép file ảnh vào thư mục đích với tên mới
+        # Sao chép file ảnh vào thư mục đích với tên gốc
         shutil.copy(file_path, destination_path)
-        
-        # Tăng số đếm
-        image_counter += 1
 
-print('Sao chép và đổi tên các ảnh đã hoàn tất.')
+print('Sao chép các ảnh đã hoàn tất.')
